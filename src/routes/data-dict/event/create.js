704e28ef5b3c2d417928f8592bc291694d3618a8
joyyieli <joyyieli@tencent.com>
2022-03-28 21:51:29 +0800

    feat: create event interface

--- src/routes/data-dict/event/create.js
+++ src/routes/data-dict/event/create.js
@@ -1,6 +1,9 @@
-module.exports = function (ctx, next) {
-  ctx;
-  next;
-  // DB.get();
-  return 'you got me in event/create!';
+const Event = require('../../../controllers/data-dict/event');
+
+module.exports = function (ctx) {
+  // GET 获取参数方式
+  // const params = ctx.query;
+  // POST获取参数方式
+  // console.log(ctx.request.body)
+  return Event.create(ctx);
 };
