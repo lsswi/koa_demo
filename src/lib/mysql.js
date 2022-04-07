fb68632069fd3a71d2ca6f73b4a4c70d1db1f6f7
joyyieli <joyyieli@tencent.com>
2022-04-07 14:49:15 +0800

    feat: delete comment

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -1,5 +1,4 @@
 const { Sequelize } = require('sequelize');
-// const md5 = require('blueimp-md5');
 
 const DBLib = {
   getDBPool() {
@@ -14,46 +13,6 @@ const DBLib = {
       timezone: '+08:00',
     });
   },
-
-  // async get() {
-  //   let abc = {
-  //     def_val: [
-  //       {
-  //         id: 1,
-  //         value: 2,
-  //       },
-  //     ],
-  //   };
-  //   let str = JSON.stringify(abc);
-  //   console.log(str);
-  //   let mdKey = md5(str);
-  //   // INSERT INTO data_dict_event (name) VALUES("hi");
-  //   // const [a, b] = await db.query("SELECT * FROM data_dict_event");
-  //   await db
-  //     // .query("DELETE FROM data_dict_event WHERE id=5")
-  //     // .query("UPDATE data_dict_event SET name = 'hhh' WHERE name = :name", {
-  //     // replacements: { name: name },
-  //     // })
-  //     .query(
-  //       "INSERT INTO data_dict_event(proto_id, name, definition_val, md_key) VALUES(1, '测试', :val, :key)",
-  //       {
-  //         replacements: { name: "abcd", val: str, key: mdKey },
-  //       }
-  //     )
-  //     // .query("SELECT * FROM data_dict_event WHERE md_key = :def", {
-  //     //   replacements: { def: name },
-  //     // })
-  //     .then(([a, b]) => {
-  //       console.log("get result a: ", a);
-  //       console.log("get type of result a: ", typeof a);
-  //       console.log("get result b: ", b);
-  //       console.log("get type of result b: ", typeof b);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err.message);
-  //     });
-  //   console.log("done in get");
-  // },
 };
 
 module.exports = DBLib;
